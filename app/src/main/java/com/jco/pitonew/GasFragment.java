d7dae756330e10770823b3676e4d5e0151b398da
josephcorbin91 <joseph.corbin91@yahoo.com>
2017-02-14 16:21:07 +0100

    Created new fragment gasDensity:

---
+++ app/src/main/java/com/jco/pitonew/GasFragment.java
@@ -0,0 +1,225 @@
+package com.jco.pitonew;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link GasFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link GasFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GasFragment extends Fragment {
+    public static GasFragment newInstance() {
+        return new GasFragment();
+    }
+
+    public double[] getStandardAirResult() {
+        return standardAirResult;
+    }
+
+    public void setStandardAirResult(double[] standardAirResult) {
+        this.standardAirResult = standardAirResult;
+    }
+
+    private double[] standardAirResult;
+
+    private TextView unitsGasDensityTemperatureDB, unitsGasDensityTemperatureWB, unitsCalculatedGasDensity, unitsGasDensitySeaLevelPressure, unitsGasDensityElevationAboveSeaLevel, unitsGasDensityAtmosphericPressure, unitsStaticPressure, unitsDuctPressure, dimensionHeader, dimension1TextView, dimension2TextView;
+
+    public Boolean getStandardAirBoolean() {
+        return standardAirBoolean;
+    }
+
+    public void setStandardAirBoolean(Boolean standardAirBoolean) {
+        this.standardAirBoolean = standardAirBoolean;
+    }
+
+    private Boolean standardAirBoolean;
+    private Switch standardAirSwitch;
+    private View mView;
+
+
+    public GasFragment() {
+
+        standardAirBoolean=false;
+
+    }
+
+
+    public void clear(){
+        Utility.clearAllFields((ViewGroup)this.mView);
+
+        View layoutDuctPressure = mView.findViewById(R.id.layoutDuctPressure);
+        layoutDuctPressure.setVisibility(View.GONE);
+        View layoutRelativeHumidity = mView.findViewById(R.id.layoutRelativeHumidity);
+        layoutRelativeHumidity.setVisibility(View.GONE);
+        View layoutAtmosphericPressure = mView.findViewById(R.id.layoutAtmosphericPressure);
+        layoutAtmosphericPressure.setVisibility(View.GONE);
+
+
+    }
+    @Nullable
+    public void showStandardAirDialog(){
+        /*
+        Refactor code so you only call the EditText Once.
+         */
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Standard Air Input");
+        builder.setView(R.layout.dialog_standardair);
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Dialog standardAirDialog = (Dialog)dialog;
+                EditText C02EditText = (EditText)standardAirDialog.findViewById(R.id.C02EditText);
+                EditText O2EditText = (EditText)standardAirDialog.findViewById(R.id.O2EditText);
+                EditText N2EditText = (EditText)standardAirDialog.findViewById(R.id.N2EditText);
+                EditText ArEditText = (EditText)standardAirDialog.findViewById(R.id.ArEditText);
+                EditText H20EditText = (EditText)standardAirDialog.findViewById(R.id.H2OEditText);
+                standardAirResult= new double[5];
+                standardAirResult[0]= Double.valueOf(C02EditText.getText().toString());
+                standardAirResult[1]= Double.valueOf(O2EditText.getText().toString());
+                standardAirResult[2]= Double.valueOf(N2EditText.getText().toString());
+                standardAirResult[3]= Double.valueOf(ArEditText.getText().toString());
+                standardAirResult[4]= Double.valueOf(H20EditText.getText().toString());
+                for(int i=0; i<standardAirResult.length; i++)
+                    System.out.print(standardAirResult[i]);
+                Toast.makeText(getActivity(), "Molecular Weight ="+String.valueOf(standardAirResult[0]*44.01/100+standardAirResult[1]*32/100+standardAirResult[2]*28.02/100+standardAirResult[3]*39.94/100+standardAirResult[4]*18.01528/100),Toast.LENGTH_LONG).show();
+
+                TextView molecularWeightTextView = (TextView)mView.findViewById(R.id.MolecularWeightTextView);
+                molecularWeightTextView.setText(String.valueOf(standardAirResult[0]*44.01/100+standardAirResult[1]*32/100+standardAirResult[2]*28.02/100+standardAirResult[3]*39.94/100+standardAirResult[4]*18.01528/100));
+
+
+
+
+
+
+
+
+
+
+            }
+        });
+
+
+        builder.setNeutralButton("Clear", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Dialog standardAirDialog = (Dialog)dialog;
+                EditText C02EditText = (EditText)standardAirDialog.findViewById(R.id.C02EditText);
+                EditText O2EditText = (EditText)standardAirDialog.findViewById(R.id.O2EditText);
+                EditText N2EditText = (EditText)standardAirDialog.findViewById(R.id.N2EditText);
+                EditText ArEditText = (EditText)standardAirDialog.findViewById(R.id.ArEditText);
+                EditText H20EditText = (EditText)standardAirDialog.findViewById(R.id.H2OEditText);
+                C02EditText.setText("");
+                O2EditText.setText("");
+                N2EditText.setText("");
+                ArEditText.setText("");
+                H20EditText.setText("");
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Dialog standardAirDialog = (Dialog)dialog;
+                standardAirDialog.hide();
+                standardAirSwitch.setChecked(false);
+
+
+            }
+        });
+        AlertDialog dialog = builder.create();
+dialog.show();
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gas_density, container, false);
+        this.mView = view;
+        standardAirSwitch = (Switch)mView.findViewById(R.id.standardAirSwitch);
+        standardAirSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    standardAirBoolean=true;
+                    showStandardAirDialog();
+                } else {
+                    Toast.makeText(getActivity(), "Molecular Weight = 28.96",
+                            Toast.LENGTH_LONG).show();
+                    standardAirBoolean=false;
+                }
+            }
+        });
+        //standardAirSwitch = (Switch)mView.findViewById(R.id.);
+
+        View layoutDuctPressure = mView.findViewById(R.id.layoutDuctPressure);
+        layoutDuctPressure.setVisibility(View.GONE);
+        View layoutRelativeHumidity = mView.findViewById(R.id.layoutRelativeHumidity);
+        layoutRelativeHumidity.setVisibility(View.GONE);
+        View layoutAtmosphericPressure = mView.findViewById(R.id.layoutAtmosphericPressure);
+        layoutAtmosphericPressure.setVisibility(View.GONE);
+        View layoutGasDensityResult = mView.findViewById(R.id.layoutGasDensity);
+        layoutGasDensityResult.setVisibility(View.GONE);
+
+
+
+        return view;
+    }
+
+    public void showResultView(){
+
+        View layoutDuctPressure = mView.findViewById(R.id.layoutDuctPressure);
+        layoutDuctPressure.setVisibility(View.VISIBLE);
+        View layoutRelativeHumidity = mView.findViewById(R.id.layoutRelativeHumidity);
+        layoutRelativeHumidity.setVisibility(View.VISIBLE);
+        View layoutAtmosphericPressure = mView.findViewById(R.id.layoutAtmosphericPressure);
+        layoutAtmosphericPressure.setVisibility(View.VISIBLE);
+        View layoutGasDensityResult = mView.findViewById(R.id.layoutGasDensity);
+        layoutGasDensityResult.setVisibility(View.VISIBLE);
+
+    }
+
+
+    public void changeUnits(String units) {
+        unitsGasDensityTemperatureDB = (TextView) mView.findViewById(R.id.UnitstemperatureGasDensityFragmentTextView);
+        unitsGasDensityTemperatureWB = (TextView) mView.findViewById(R.id.UnitsWetBulbtemperatureGasDensityFragmentTextView);
+        unitsGasDensitySeaLevelPressure = (TextView) mView.findViewById(R.id.UnitsseaLevelPressureGasDensityFragmentTextView);
+        unitsGasDensityElevationAboveSeaLevel = (TextView) mView.findViewById(R.id.UnitsElevationAboveSeaLevelFragmentTextView);
+        unitsGasDensityAtmosphericPressure = (TextView) mView.findViewById(R.id.UnitsAtmosphericPressureFragmentTextViewUnits);
+        unitsDuctPressure = (TextView) mView.findViewById(R.id.UnitsDuctPressureFragmentTextView);
+        unitsCalculatedGasDensity = (TextView) mView.findViewById(R.id.UnitsCalculatedGasDensityTextView);
+        switch (units) {
+            case "Metric":
+                unitsGasDensityTemperatureDB.setText("°C");
+                unitsGasDensityTemperatureWB.setText("°C");
+                unitsGasDensitySeaLevelPressure.setText("kPa");
+                unitsGasDensityElevationAboveSeaLevel.setText("m");
+                unitsGasDensityAtmosphericPressure.setText("kPa");
+                unitsDuctPressure.setText("kPa");
+                unitsCalculatedGasDensity.setText("kg/m³");
+                break;
+            case "Imperial":
+                unitsGasDensityTemperatureDB.setText("°F");
+                unitsGasDensityTemperatureWB.setText("°F");
+                unitsGasDensitySeaLevelPressure.setText("in. Hg");
+                unitsGasDensityElevationAboveSeaLevel.setText("ft");
+                unitsGasDensityAtmosphericPressure.setText("in. Hg");
+                unitsDuctPressure.setText("in. Hg");
+                unitsCalculatedGasDensity.setText("lb/ft³");
+                break;
+        }
+    }
+}
